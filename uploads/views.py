cd2307f122122cc41fdab5aba18374b9192b3584
shoemonkey <romanr32@gmail.com>
2015-03-19 09:44:45 -0500

    datatables working!?!

--- uploads/views.py
+++ uploads/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from uploads.models import Part
 
+from datatableview.views import DatatableView
+
 # Create your views here.
 
-class PartListView(ListView):
+class PartListView(DatatableView):
     model = Part
 
 part_list = PartListView.as_view()
