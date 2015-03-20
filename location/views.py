f9a9f1da16bc8c7db0b0fd4bb1659fb28228d68a
shoemonkey <romanr32@gmail.com>
2015-03-20 16:19:23 -0500

    location datatable init

--- location/views.py
+++ location/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 from django.views.generic import ListView
 
-from location.models import  JMWLocationInfo 
+from location.models import  JMWLocationInfo
+from datatableview.views import DatatableView
 
 # Create your views here.
 
-class LocationListView(ListView):
+class LocationListView(DatatableView):
     model = JMWLocationInfo 
 
 location_list = LocationListView.as_view()
