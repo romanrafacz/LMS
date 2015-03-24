da8b512709322a297a1d93511acbec099c07dc8d
shoemonkey <romanr32@gmail.com>
2015-03-23 19:24:40 -0500

    speaker admin fixed

--- speaker/admin.py
+++ speaker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from speaker.models import SpeakerInfo
+
 # Register your models here.
+
+@admin.register(SpeakerInfo)
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email', 'mobile_phone', 'city', 'state')
