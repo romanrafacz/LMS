159c81798784d5cf9932c24dbc98e5b318ff78eb
shoemonkey <romanr32@gmail.com>
2015-03-24 13:08:04 -0500

    schedule datatables done

--- schedule/views.py
+++ schedule/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, ListView
+from datatableview.views import DatatableView
 
-from photos.models import Photo
+from lms.models import JMWClassInfo 
 
 # Create your views here.
-class ScheduleListView(TemplateView):
+class ScheduleListView(DatatableView):
     template_name = 'schedule/schedule_list.html'
+    model = JMWClassInfo
+    datatable_options = {
+        'columns': [
+            "avnet_type_name", "jmw_name", "start_date", "end_date", "duration", "public", "class_status", "total_registered", "instructor_name", "tech", "labs"
+        ]
+    }
 
 
 schedule_list = ScheduleListView.as_view()
