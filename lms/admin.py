11dc48cbd86f604aaa2b66b950f58414ef54624e
shoemonkey <romanr32@gmail.com>
2015-03-27 09:36:07 -0500

    deleted some files

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from lms.models import JMWClassInfo
 # Register your models here.
+
+@admin.register(JMWClassInfo)
+class JMWClassAdmin(admin.ModelAdmin):
+    list_display = ('jmw_name', 'avnet_type_name', 'start_date', 'end_date', 'duration', 'public', 'class_status',
+                    'total_registered', 'instructor_name', 'tech', 'labs')
