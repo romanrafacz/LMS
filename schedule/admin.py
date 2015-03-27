69c79a49e18d3c8604920c17ba0d0475027b999f
shoemonkey <romanr32@gmail.com>
2015-03-26 20:18:35 -0500

    login/logout added

--- schedule/admin.py
+++ schedule/admin.py
@@ -5,4 +5,5 @@ from lms.models import JMWClassInfo
 
 @admin.register(JMWClassInfo)
 class JMWClassAdmin(admin.ModelAdmin):
-    list_display = ('jmw_name', 'avnet_name', 'start_date', 'end_date', 'duration', 'public', 'class_status', 'total_registered', 'instructor_name', 'tech', 'labs')
+    list_display = ('jmw_name', 'avnet_type_name', 'start_date', 'end_date', 'duration', 'public', 'class_status',
+                    'total_registered', 'instructor_name', 'tech', 'labs')
