0e2364305db0ca779042b9e9c6bb96919862008f
shoemonkey <romanr32@gmail.com>
2015-03-31 15:51:23 -0500

    sign up form validation

--- registration/views.py
+++ registration/views.py
@@ -21,15 +21,20 @@ def register(request):
     return render(request, 'registration/sign_up.html', {})
 
 def complete_signup(request):
-    userform = UserForm(data=request)
+
 
     if request.method == 'POST':
-        message = 'success'
-        user = User.objects.create_superuser(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
-        user.save()
-        return render(request, 'registration/registration_response.html', {'message':message})
+        userform = UserForm(data=request.POST)
+        if userform.is_valid():
+            message = 'success'
+            user = User.objects.create_superuser(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
+            user.save()
+            return render(request, 'registration/registration_response.html', {'message':message})
+        else:
+            message = 'Please fill out all the fields'
+            return render(request, 'registration/complete_signup.html', {'message': message, 'userform': userform})
 
-    userform = UserForm(data=request)
+    userform = UserForm()
     return render(request, 'registration/registration_response.html', {'userform':userform, 'message':message})
 
 
