850091e894865ecc5b28f33b0a594b4c96990e98
shoemonkey <romanr32@gmail.com>
2015-04-03 14:51:43 -0500

    eew report base, params from view

--- reporting/views.py
+++ reporting/views.py
@@ -5,6 +5,13 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class ReportView(TemplateView):
+
     template_name = 'reporting/report_view.html'
+
+    def get_context_data(self, **kwargs):
+        jm_2013 = 256000
+        jm_2014 = 315000
+        jm_2015 = 141200
+        return {'jm_2013':jm_2013, 'jm_2014': jm_2014, 'jm_2015': jm_2015}
 
 reporting_view = ReportView.as_view()
