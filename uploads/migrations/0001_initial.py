5d5d6105bc3f427b5bda4887b7538baec37c2fb2
shoemonkey <romanr32@gmail.com>
2015-04-09 11:55:49 -0500

    migrations removed - altered models

--- uploads/migrations/0001_initial.py
+++ uploads/migrations/0001_initial.py
@@ -14,12 +14,14 @@ class Migration(migrations.Migration):
             name='Part',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('training_id', models.CharField(max_length=25)),
+                ('jmw_name', models.CharField(max_length=20)),
                 ('name', models.CharField(max_length=255)),
                 ('company', models.CharField(max_length=50)),
+                ('email', models.CharField(max_length=100)),
                 ('phone', models.CharField(max_length=25)),
             ],
             options={
+                'abstract': False,
             },
             bases=(models.Model,),
         ),
