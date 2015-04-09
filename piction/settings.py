355cd2a1cd036a88e54ec0acf0d961e2d6bfd210
shoemonkey <romanr32@gmail.com>
2015-04-09 14:09:46 -0500

    migration/syndb working

--- piction/settings.py
+++ piction/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'piction',
+        'NAME': 'test_piction',
         'USER': 'roman',
         'PASSWORD': 'snickers',
         'HOST': '192.168.0.7'
--- uploads/migrations/0001_initial.py
+++ uploads/migrations/0001_initial.py
@@ -7,6 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('lms', '0001_initial'),
     ]
 
     operations = [
@@ -14,11 +15,11 @@ class Migration(migrations.Migration):
             name='Part',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('jmw_name', models.CharField(max_length=20)),
                 ('name', models.CharField(max_length=255)),
                 ('company', models.CharField(max_length=50)),
                 ('email', models.CharField(max_length=100)),
                 ('phone', models.CharField(max_length=25)),
+                ('jmw_name', models.ForeignKey(to='lms.JMWClassInfo')),
             ],
             options={
                 'abstract': False,
