982922e6ebcbc9924639d1039eae43a93ec2414f
shoemonkey <romanr32@gmail.com>
2015-04-20 10:28:28 -0500

    schedule views has request/json libraries - need api info

--- schedule/views.py
+++ schedule/views.py
@@ -5,6 +5,10 @@ from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, ListView
 from datatableview.views import DatatableView
 
+##import libraries for api pull
+import requests
+import json
+
 from lms.models import JMWClassInfo, ClassPlSummary 
 
 # Create your views here.
@@ -16,6 +20,10 @@ class ScheduleListView(DatatableView):
             "avnet_type_name", "jmw_name", "start_date", "end_date", "duration", "public", "class_status", "total_registered", "instructor_name", "tech", "labs"
         ]
     }
+
+    def get_context_data(self, **kwargs):
+        schedule_data = ""
+        return {'schedule_data': schedule_data}
 
 
 schedule_list = ScheduleListView.as_view()
